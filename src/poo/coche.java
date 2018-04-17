4a66aa5ac9f37aab30f0905402e66d61a8e35d9d
JonathanAlberto <jona_chivas_4000@hotmail.com>
2018-04-16 21:44:18 -0500

    Clases y Objetos

---
+++ src/poo/coche.java
@@ -0,0 +1,18 @@
+package poo;
+
+
+public class coche {
+    int rueda;
+    int motor;
+    int peso;
+    int largo;
+    int ancho;
+    
+    public coche(){ //Metodo constructor: inicializa los valores de la clase
+        rueda=4;
+        largo=2000;
+        ancho=300;
+        motor=1600;
+        peso=500;
+    }
+}
---
+++ src/poo/usocoche.java
@@ -0,0 +1,9 @@
+
+package poo;
+
+public class usocoche {
+    public static void main (String arg[]){
+        coche Nissan= new coche();
+        System.out.println("El Coche Cuenta Con: "+Nissan.rueda);
+    }
+}
