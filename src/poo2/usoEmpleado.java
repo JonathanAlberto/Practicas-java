f0f0e55bd8c5e2446fb8d5629e60905530072861
JonaColombiano <metalmonsoon.75@gmail.com>
2018-04-29 19:10:16 -0500

    Polimorfismo y enlazado Dinamico

--- src/poo2/usoEmpleado.java
+++ src/poo2/usoEmpleado.java
@@ -10,43 +10,33 @@ package poo2;
  * @author Aspire 5
  */
 public class usoEmpleado {
-
-
-    public static void main(String[] args) {
-     Empleado[] empleado=new Empleado[4];
+ public static void main(String[] args) {
+     Jefatura jefeRH=new Jefatura("Susan", 10000,2012,01,07);
+     jefeRH.establecerIncentivo(5);
+     Empleado[] empleado=new Empleado[6];
      empleado[0]=new Empleado("Jose", 1500,2012,01,02);
      empleado[1]=new Empleado("Maria", 1500,2012,06,15);
      empleado[2]=new Empleado("Ivan", 1500,2013,05,07);
      empleado[3]=new Empleado("Juan");//Se llama al segundo constructor que solo recibe un parametro
+     empleado[4]=jefeRH;              //Polimorfismo en accion. Principio de sustitucion
+     empleado[5]=new Jefatura ("Susan", 10000,2012,01,07);
+     //la clase que hereda toma las variables objeto del padre como si fueran suyas
+     //En eso consiste el polimorfismo
+
      
      for(Empleado e: empleado){
          e.SubeSueldo(5);
      }
      
      for(Empleado x: empleado){
-         System.out.println(x.obtenerNombre()+" "+ x.obtenerSueldo()+" "+x.obtenerFechaContrato());
+         System.out.println(x.obtenerNombre()+" "+
+                 x.obtenerSueldo()+" "+    //Enlazado Dinamico: la Maquina virtual detecta el metodo correspondiete
+                                           //y utiliza el metodo que sobrescribimos solo con la clase heredada
+                 x.obtenerFechaContrato());
      }
     }
     
     
-}
-//HERENCIA
-class Jefatura extends Empleado{//Heredamos de la clase padre (Empleado) con la palabra reservada Extends
-    private double incentivo;
-    public Jefatura(String nom, double sue, int agno, int mes, int dia){//Recibimos parametros
-    super(nom, sue, agno,mes,dia); //Con super hacemos referencia al constructor del padre, al cual se enviamos los parametros
-}
-    
-    public void establecerIncentivo(double x){//metodo Setter
-        incentivo=x;
-    }
-    
-    //sobreescribimos el metodo de la clase padre
-    //se olvida del metodo de la clase padre para esta clase y toma por predeterminado este
-    public double obtenerSueldo(){
-        double sueldo= incentivo+super.obtenerSueldo();//llamamos al metodo obtner sueldo de la clase padre
-        return sueldo;
-    }
-}
 
-
+    
+}
